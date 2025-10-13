8ecb601eacf4db2a684ee6684f6b16dc21cd34ae
dingjinghui <dingjinghui33@163.com>
2025-10-13 15:15:12 +0800

    feat: 首页增加公开文件展示

--- src/djhx_pan/route/main.py
+++ src/djhx_pan/route/main.py
@@ -1,9 +1,12 @@
+import datetime
 import logging
 
 from flask import Blueprint, render_template, session
 
+from .file import ICON_TYPES, _row_to_dict
 from ..config.app_config import AppConfig
 from ..util import login_required
+from ..db import DB
 
 main_bp = Blueprint('main', __name__)
 app_logger = logging.getLogger(AppConfig.PROJECT_NAME + "." + __name__)
@@ -17,7 +20,25 @@ def index():
     if s_user:
         username = s_user.get('username')
 
-    return render_template('index.html', username=username)
+    public_shares = DB.query("""
+        SELECT
+            t_share.id as share_id,
+            t_file.id as file_id,
+            *
+        FROM t_share 
+        JOIN t_file ON t_share.file_id = t_file.id
+        WHERE password IS NULL AND (expires_at IS NULL OR expires_at > ?) AND t_file.is_dir = 0
+    """, (datetime.datetime.now().isoformat(), ))
+
+    public_shares = [_row_to_dict(r) for r in public_shares] if public_shares else []
+
+    for f in public_shares:
+        f['icon_class'] = 'folder' if f['is_dir'] else ICON_TYPES.get((f.get('filetype') or '').lower(), 'file')
+    public_shares.sort(key=lambda r: (not r['is_dir'],
+                              -datetime.datetime.fromisoformat(r['create_datetime']).timestamp() if r[
+                                  'create_datetime'] else 0))
+
+    return render_template('index.html', username=username, files=public_shares)
 
 
 @main_bp.get('/dashboard')
